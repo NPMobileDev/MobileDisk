d89fc4e6df84f4645b359c8cdd3a778e6d15c6c7
tomneo2004 <nelsonmac-mini@nelsonserver.local>
2012-07-23 10:04:26 +0800

    code modify

--- MobileDisk/MDSettingsViewController.m
+++ MobileDisk/MDSettingsViewController.m
@@ -361,6 +361,7 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
     NSString *ipStr = nil;
     NSString *httpAddress;
     
+    /*
     for(int i=0; i<4 ; i++)
     {
         ipStr = [IPs objectForKey:[NSString stringWithFormat:@"en%i", i]];
@@ -370,6 +371,17 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
             break;
         }
     }
+     */
+    
+    int i =0;
+    
+    do {
+        
+        ipStr = [IPs objectForKey:[NSString stringWithFormat:@"en%i", i]];
+        
+        i++;
+        
+    } while (ipStr == nil);
     
     if(ipStr != nil)
     {
